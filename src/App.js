6c2f880fd5ca09fb6aacc5ceb488f87c7fe924ab
Khairul Ashry Sabry <ashry@KISWIRE.MY>
2020-02-05 15:48:31 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="small">404</h1>
+        <h3>The requested page cannot be found</h3>
+        <Button variant="contained" color="primary" disableElevation>
+          Contact EDP
+        </Button>
       </header>
     </div>
   );
