70ba5d31143ec70f47fba0f492e30e29c4c7bd2e
Khairul Ashry Sabry <ashry@KISWIRE.MY>
2020-02-10 16:33:14 +0800

    add smiley

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import sad from './sad-face.png';
 import './App.css';
-import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
+    <div className="App1">
+      <div className="left">
+      <img src={sad} className="sad"/>
+      </div>
+      <div className="right">
         <h1 className="small">404</h1>
         <h3>The requested page cannot be found</h3>
-        <Button variant="contained" color="primary" disableElevation>
-          Contact EDP
-        </Button>
-      </header>
+        <div>
+          <button className="button">
+            Contact EDP
+          </button>
+          <button className="button">
+            Home
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
